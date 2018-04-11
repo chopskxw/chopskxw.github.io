d4101daa1d70b2957e74b3943ccf9d276ab91371
Roy Williams <roywilli@roywilli.redhat.com>
2018-04-11 18:42:03 -0400

    First working prototype.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Roy G. Williams'
 SITENAME = u'Roy G. Williams'
-SITEURL = 'chopskxw.github.io'
+SITEURL = ''
 
 PATH = 'content'
 
@@ -37,5 +37,22 @@ SOCIAL = (
 DEFAULT_PAGINATION = 10
 THEME = "pelican-theme"
 
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['gravatar',]
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# lannisport settings
+SITELOGO = 'images/gravatar.jpeg'
+SITETAGLINE = 'Father, Husband, Geek, and Firefighter'
+SITEDESCR = 'By day I am I am the lonely Red Hatter of Southern WV. Outside of business hours, I advocate for FOSS, play with new tech, operate amature radios, and play firefighter--sometimes they even let me teach.'
+GITHUB_URL = 'http://github.com/chopskxw'
+LINKEDIN_URL = 'http://linkedin.com/in/roygwilliams'
+TWITTER_URL = 'http://twitter.com/roygwilliams'
+FACEBOOK_URL = 'http://facebook.com/roygwilliams'
+LICENSE_NAME = 'GPL v3'
+LICENSE_URL = 'https://github.com/chopskxw/chopskxw.github.io/blob/master/LICENSE'
+
+# gravatar
+AUTHOR_EMAIL = 'roygwilliams.kxw@gmail.com'
