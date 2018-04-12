8db721ade57cea93ed5dfd5f26992bcccbb5992e
Roy Williams <roywilli@roywilli.redhat.com>
2018-04-12 15:54:48 -0400

    getting a fresh start

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,62 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-TYPOGRIFY = True
-
 AUTHOR = u'Roy G. Williams'
 SITENAME = u'Roy G. Williams'
 SITEURL = 'https://chopskxw.github.io'
 
-# lannisport settings
-SITELOGO = 'images/me.png'
-SITETAGLINE = u'Father, Husband, Geek, and Firefighter'
-SITEDESCR = u'By day I am I am the lonely Red Hatter of Southern WV. Outside of business hours, I advocate for FOSS, play with new tech, operate amateur radios, and play firefighter--sometimes they even let me teach.'
-GITHUB_URL = 'http://github.com/chopskxw'
-LINKEDIN_URL = 'http://linkedin.com/in/roygwilliams'
-TWITTER_URL = 'http://twitter.com/roygwilliams'
-FACEBOOK_URL = 'http://facebook.com/roygwilliams'
-LICENSE_NAME = 'GPL v3'
-LICENSE_URL = 'https://github.com/chopskxw/chopskxw.github.io/blob/master/LICENSE'
-
-THEME = "pelican-theme"
-MAIN_MENU = True
-#PLUGIN_PATHS = ['pelican-plugins']
-#PLUGINS = ['gravatar',]
+PATH = 'content'
 
 TIMEZONE = 'America/New_York'
+
 DEFAULT_LANG = u'en'
 
-DEFAULT_PAGINATION = 10
-PATH = 'content'
-STATIC_PATHS = [
-    'files',
-    'images',
-    'cv',
-]
-
-ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
-ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
-
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = PAGE_URL + 'index.html'
-
-CATEGORY_URL = 'category/{slug}/'
-CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
-
-TAG_URL = 'tag/{slug}/'
-TAG_SAVE_AS = TAG_URL + 'index.html'
-
-ARCHIVES_URL = 'archives/'
-ARCHIVES_SAVE_AS = ARCHIVES_URL + 'index.html'
-
-CONTACT_URL = 'pages/contact/'
-
 # Feed generation is usually not desired when developing
-#FEED_ALL_ATOM = None
-#CATEGORY_FEED_ATOM = None
-#TRANSLATION_FEED_ATOM = None
-#AUTHOR_FEED_ATOM = None
-#AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
-# gravatar
-#AUTHOR_EMAIL = 'roygwilliams.kxw@gmail.com'
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
